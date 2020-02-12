96d87c44eab75f54ae00030dffd641ea3facf7a5
June Castillote <june.castillote@gmail.com>
2020-02-12 12:29:42 +0800

    update

---
+++ CertX.ps1
@@ -0,0 +1,60 @@
+[cmdletbinding()]
+param (
+    <#
+        This parameter accepts the list (array) of servers to be checked.
+    #>
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [array]
+    $ServerList,
+
+    <#
+        This parameter indicates to return on certificates that are expiring in X days.
+        If not used, all certificates in LocalMachine\My will be reported
+    #>
+    [Parameter()]
+    [int[]]
+    $ExpiringInDays
+)
+
+$today = Get-Date
+$finalResult = @()
+
+foreach ($server in $ServerList) {
+
+    $certObjectList = Invoke-Command -ComputerName ($server) -ScriptBlock {
+        Get-ChildItem "Cert:\LocalMachine\My"
+    }
+
+    if ($certObjectList) {
+        foreach ($certObject in $certObjectList) {
+
+            # Calculate
+            $DaysLeft = (New-TimeSpan -Start $today -End ($certObject.NotAfter)).Days
+
+            $tempObj = [ordered]@{
+                PSTypeName                = "LazyExchangeAdmin.Certificate.Report"
+                "Server Name"             = $server
+                "Certificate Name"        = ($certObject.Subject)
+                "Certificate Thumbprint"  = ($certObject.Thumbprint)
+                "Certificate Valid From"  = ($certObject.NotBefore)
+                "Certificate Valid Until" = ($certObject.NotAfter)
+                "Day(s) Remaining"        = $DaysLeft
+            }
+
+            if (!$ExpiringInDays) {
+                $finalResult += New-Object psobject -property $tempObj
+            }
+
+            if ($ExpiringInDays) {
+                foreach ($day in $ExpiringInDays) {
+                    if ($DaysLeft -eq $day) {
+                        $finalResult += New-Object psobject -property $tempObj
+                    }
+                }
+            }
+        }
+    }
+}
+return $finalResult
+
